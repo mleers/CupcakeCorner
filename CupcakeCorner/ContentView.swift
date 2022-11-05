3f5d2be1cb8af7e805f0b2020ccc744fcb00412c
mleers <mleers4@gmail.com>
2022-11-04 17:35:46 -0700

    Initial Commit

---
+++ CupcakeCorner/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  CupcakeCorner
+//
+//  Created by mitch on 11/4/22.
+//  Copyright © 2022 mleers. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,18 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by mitch on 11/4/22.
+//  Copyright © 2022 mleers. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
