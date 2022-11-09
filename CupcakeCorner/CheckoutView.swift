57e7c2b7caa216b56fcb9f9811aa6abfa5bdc369
mleers <mleers4@gmail.com>
2022-11-08 20:31:53 -0800

    Add data encoding and checkout logic

--- CupcakeCorner/CheckoutView.swift
+++ CupcakeCorner/CheckoutView.swift
@@ -11,6 +11,9 @@ import SwiftUI
 struct CheckoutView: View {
     @ObservedObject var order: Order
     
+    @State private var confirmationMessage = ""
+    @State private var showingConfirmation = false
+    
     var body: some View {
         ScrollView {
             VStack {
@@ -26,12 +29,43 @@ struct CheckoutView: View {
                 Text("Your cost is: \(order.cost, format: .currency(code: "USD"))")
                     .font(.title)
                 
-                Button("Place order", action: {})
-                    .padding()
+                Button("Place order") {
+                    Task {
+                        await placeOrder()
+                    }
+                }
+                .padding()
             }
         }
         .navigationTitle("Check out")
         .navigationBarTitleDisplayMode(.inline)
+        .alert("Thank you!", isPresented: $showingConfirmation) {
+            Button("OK") { }
+        } message: {
+            Text(confirmationMessage)
+        }
+    }
+    
+    func placeOrder() async {
+        guard let encoded = try? JSONEncoder().encode(order) else {
+            print("Failed to encode order")
+            return
+        }
+        
+        let url = URL(string: "https://reqres.in/api/cupcakes")!
+        var request = URLRequest(url: url)
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        
+        do {
+            let (data, _) = try await URLSession.shared.upload(for: request, from: encoded)
+            
+            let decodedOrder = try JSONDecoder().decode(Order.self, from: data)
+            confirmationMessage = "Your order for \(decodedOrder.quantity)x \(Order.types[decodedOrder.type].lowercased()) cupcakes is on its way!"
+            showingConfirmation = true
+        } catch {
+            print("Checkout failed.")
+        }
     }
 }
 
